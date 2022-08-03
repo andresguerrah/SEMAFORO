4dee91963df0b7f3b4a8833459c6fe97f10eac72
andresguerrah <andres.guerrah@gmail.com>
2022-08-03 18:27:03 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,15 +13,15 @@ const OnOff = () => {
     return (
 	<div className="semaforo">
 		<div className="red">
-        	<button class={ opened ? "redon" : "redoff"} type="button" onClick={() => {setOpened(!opened); opened && (setOpened2(false), setOpened3(false))}}></button>
+        	<button class={ opened ? "redon" : "redoff"} type="button" onClick={() => {setOpened(!opened);   (setOpened2(false), setOpened3(false))}}></button>
     	</div>
 			
 		<div className="yellow">
-        	<button class={ opened2 ? "yellowon" : "yellowoff"} type="button" onClick={() => {setOpened2(!opened2); opened2 && (setOpened(false), setOpened3(false))}}></button>
+        	<button class={ opened2 ? "yellowon" : "yellowoff"} type="button" onClick={() => {setOpened2(!opened2);   (setOpened(false), setOpened3(false))}}></button>
 	    </div>
 
 	    <div className="green">
-        	<button class={ opened3 ? "greenon" : "greenoff"} type="button" onClick={() => {setOpened3(!opened3); opened3 && (setOpened(false), setOpened2(false))}}></button>
+        	<button class={ opened3 ? "greenon" : "greenoff"} type="button" onClick={() => {setOpened3(!opened3);   (setOpened(false), setOpened2(false))}}></button>
 	    </div>
 	</div>
 	);
